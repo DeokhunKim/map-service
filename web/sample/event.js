e25f2dcf167d779eff3cd62e7195496d0cb92bf2
thekoon <deokhun.kim@mnc.ai>
2024-04-15 17:11:46 +0900

    add rightclick event

--- web/sample/event.js
+++ web/sample/event.js
@@ -1,15 +1,28 @@
-console.log('index2')
 import { map } from './index.js';
 import { create_place } from './marker.js';
+import { create_popup_select, hide_popup_select } from "./draw.js";
 
 
+let status = 'stand_by'
 
 
 
 map.addListener("click", (e) => {
-	let position = e.latLng.toJSON();
-	console.log(position);
-	create_place(position.lat, position.lng, "basic")
+	if(status === 'stand_by') {
+		console.log('status is stand_by');
+		let position = e.latLng.toJSON();
+		create_place(position.lat, position.lng, "basic")
+	}
+	else if(status === 'popup_select') {
+		console.log('status is popup_select');
+		hide_popup_select();
+	}
+
   });
-  
-  
+
+
+map.addListener("rightclick", (e) => {
+	let position = e.latLng.toJSON();
+	create_popup_select(position.lat, position.lng, map)
+	status = 'popup_select'
+  });
--- web/sample/index.js
+++ web/sample/index.js
@@ -1,3 +1,5 @@
+export { map };
+
 const { Map } = await google.maps.importLibrary("maps");
 let map = new Map(document.getElementById("map"), {
     center: new google.maps.LatLng(36.589979, 127.9716999),
@@ -10,7 +12,7 @@ async function initMap() {
 }
 
 initMap();
-export { map };
+
 console.log('initMap');
 
 
